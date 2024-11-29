513c9f993e3fe8b6807318557d3e9c2a618e5afe
Meg Grasse <meg@Megs-MacBook-Air.local>
2024-11-29 17:34:53 +0000

    cleaned up code comments for collaboration.

--- landfill.m
+++ landfill.m
@@ -1,9 +1,7 @@
 % this code calculates the profit for the different permutations of a 
 % composite liner system. 
-% the next step is to explore if increasing thickness could increase profit
-% by decreasing leachate.
 
-% the parameters to calculating `q` using the composite liner equation
+% the parameters to calculating `Q` using the composite liner equation
 % for each permuation of the composite liner system (high vs. low quality
 % clay and good vs. bad contact]
 %
@@ -13,47 +11,66 @@
 % c: factor per good contact/poor contact
 % a: hole area (m^2)
 % k: hydraulic conductivity of mineral layer
-q_params = [ 20 1.15 0.05 1*10^-8 
-      5 0.21 0.01 1*10^-8 
-      20 1.15 0.05 1*10^-9
-      5 0.21 0.01 1*10^-9 
+Q_params = [ 20 1.15 0.05 1*10^-8 %poor contact, poor mineral layer
+      5 0.21 0.01 1*10^-8 %good contact, poor mineral layer
+      20 1.15 0.05 1*10^-9 %poor contact, good mineral layer
+      5 0.21 0.01 1*10^-9 %good contact, good mineral layer
     ];
 
 area_m2 = 10000;
-% q (m^2/s) for each lining option
-q = (Q_params(:,1).*Q_params(:,2).*1.^0.9.*Q_params(:,3).^0.1.*Q_params(:,4).^0.74)/area_m2;
+% Q (m^3/s) for each lining option.
+% implements the `Q=n*0.21(h^0.9*a^0.1*k^0.74)` [poor contact] or
+% `Q=n*0.21(h^0.9*a^0.1*k^0.74)` [good contact] G&B (1989) equation.
+Q = (Q_params(:,1).*Q_params(:,2).*1.^0.9.*Q_params(:,3).^0.1.*Q_params(:,4).^0.74);
 
 seconds_per_year = 365*24*60*60;
 
-% parameters to calculate cost. common sense assumption as LLDPE as the
-% geomembrane (cheapest) and that composite liners require a protection
-% layer.
-
+% parameters to calculate cost, following the same row structure as
+% `Q_params` for the different options.
+% 
+% assuming that a composite liner must require a protection layer.
+%
 % params:
-% [protection, geomembrane, clay, leachate]
-C_params = [ 4 8 20 0 300*60*q(1)*seconds_per_year
-      4 8 20 50 300*60*q(2)*seconds_per_year
-      4 8 100 0 300*60*q(3)*seconds_per_year
-      4 8 100 50 300*60*q(4)*seconds_per_year
+% [pl, gm, gml, cqa, l]
+% pl: cost of protection layer (per m^2)
+% gm: cost of geomembrane (per m^2) [using LLDPE for now as it's cheapest]
+% ml: cost of mineral layer (per m^2)
+% cqa: cost of CQA (per m^2)
+% l: cost of pumping and treating leakage. calculated by multiplying
+% quanitity (flow rate x time) by cost (per landfill area)
+
+landfill_lifetime_yrs = 60;
+C_params = [ 4 8 20 0 landfill_lifetime_yrs*seconds_per_year*Q(1)*300 %poor contact, poor mineral layer
+      4 8 20 50 landfill_lifetime_yrs*seconds_per_year*Q(2)*300 %good contact, poor mineral layer
+      4 8 100 0 landfill_lifetime_yrs*seconds_per_year*Q(3)*300 %poor contact, good mineral layer
+      4 8 100 50 landfill_lifetime_yrs*seconds_per_year*Q(4)*300 %good contact, good mineral layer
     ];
+% convert all costs to cost per landfill area 
+C_params = [C_params(:,1:4).*area_m2, C_params(:,5)];
 
 clay_thickness = 1;
+% add up the costs for each option, multiplying by 1m of clay thickness
 C = C_params(:,1)+C_params(:,2)+C_params(:,3)*clay_thickness+C_params(:,4)+C_params(:,5);
 
 % income assuming settlement to meet regulation
-I=7.50*(30-1)/.8;
+I=7.50*(30-1)/.8 * area_m2;
 
 %profit
 P=I-C
 
-% quick expl. of mineral layer only
-% thickness 1m
-Q_min=10^-9*(1+1)/1;
-% high quality clay, leakage
-C_min=100*Q_min*(300*60*365*24*60*60);
+% this is a quick exploration of just using a 1m "good quality" clay layer 
+% with 10^-9 permeability; the mininmum needed to achieve the landfill 
+% directive.
+
+% calculate hydraulic gradient via `i=h+D/D` where h=1m and D(thickness)=1m
+i_ml = 1+1/1;
+% calcuate's darcy's law: q=k*i*a 
+Q_good_ml=10^-9*i_ml*area_m2;
+% cost: cost of mineral layer + leachate quanitity cost
+C_good_ml=100+area_m2 + landfill_lifetime_yrs*seconds_per_year*Q_good_ml*300;
 % income per m2
-I_min=7.50*(30-1)/.8;
-P_min = I_min-C_min;
+I_good_ml=7.50*(30-1)/.8 * area_m2;
+P_good_ml = I_good_ml-C_good_ml
 
 % next steps:
 % 1. explore increasing thickness with additional layers for current
