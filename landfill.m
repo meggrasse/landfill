0ca9fb5628a773118554264f7d1ceb899fd33440
Meg Grasse <meggrasse@gmail.com>
2024-12-13 00:00:39 +0000

    update cover soil cost

--- landfill.m
+++ landfill.m
@@ -134,7 +134,7 @@ capping_cost = capping_cost*AREA_M2;
 CappingCost = ones(indexes, 1);
 CappingCost = CappingCost.*capping_cost;
 
-cover_soil_cost = 49000;
+cover_soil_cost = 49833;
 CoverSoilCost = ones(indexes, 1);
 CoverSoilCost = CoverSoilCost.*cover_soil_cost;
 
